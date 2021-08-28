7c340e4a01a88f13bf722ad0fcc0681bec27af4c
Yuichi Kanno <fksmhr077@gmail.com>
2021-08-28 20:06:48 +0900

    store の接続

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HomeContainer as Home } from "../src/containers/Home";
+import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Provider store={store}>
+			<div className="App">
+				<Home />
+			</div>
+		</Provider>
+	);
 }
 
 export default App;
